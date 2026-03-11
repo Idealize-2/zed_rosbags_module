6c6dcba57ab0b6760e218ca6629a4c48e6d503a4
walkie <walkie@walkie.com>
2026-03-11 22:09:56 +0700

    fix/change to load data in chunks

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 from zed_utils import ZEDBagReader
 import cv2
 
+BAG_PATH = '/home/walkie/robocup2026/rosbag/2026-03-06-14-36-20'
+
 def main():
-    reader = ZEDBagReader('/home/walkie/walkie-projects/walkie-ros-ws/rosbag/2026-03-04-18-07-59-Sim')
+    reader = ZEDBagReader(BAG_PATH)
     for time, frame in reader.images:
         cv2.imshow('Image', frame)
         print(f"Time: {time}")
 
-
-            
         if cv2.waitKey(1) & 0xFF == ord('q'): # wait 33 key for framerate purpose
             break
-            
+
 if __name__ == "__main__":
     main()
